22b453c264783732e6e71a6e7d2a8fe9e838d443
FelipeSulser <felipesulser@gmail.com>
2020-11-06 10:48:53 +0100

    new changes on voice recognition

--- voice_parser.py
+++ voice_parser.py
@@ -4,39 +4,20 @@ import speech_recognition as sr
 		- pyaudio
 		- speech_recognition
 """
+class Listener():
+	def __init__(self):
+		self.recognizer = sr.Recognizer()
+		
 
-def extract_text():
-	text = None
-	r = sr.Recognizer()
-	with sr.Microphone() as source:
-		r.adjust_for_ambient_noise(source)
-		print("Please speak")
-		audio = r.listen(source)
-		try:
-			text = r.recognize_google(audio)
-			print("Text recognized: \n" + text)
-		except Exception as e:
-			print("Error " + str(e))
-	return text
-
-
-def listen_command():
-	r = sr.Recognizer()
-	with sr.Microphone() as source:
-		r.adjust_for_ambient_noise(source)
-		print("Please speak")
-		audio = r.listen(source)
-		print("Recognizing")
-		try:
-			print("You said \n" + r.recognize_google(audio))
-			print("Audio recorded succesfully")
-
-		except Exception as e:
-			print("Error " + str(e))
-
-		with open("recorded.wav", "wb") as f:
-			f.write(audio.get_wav_data())
-
-
-if __name__ == "__main__":
-    listen_command()
+	def extract_text(self):
+		text = ""
+		with sr.Microphone() as source:
+			self.recognizer.adjust_for_ambient_noise(source)
+			try:
+				print("Please speak")
+				audio = self.recognizer.listen(source)
+				text = self.recognizer.recognize_google(audio)
+				print("Text recognized: \n" + text)
+			except Exception as e:
+				print("Error " + str(e))
+		return text
