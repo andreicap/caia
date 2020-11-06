9e75a33a331a6919c50527e00e1c4339574107d6
andreicap <andrei.capastru@gmail.com>
2020-11-06 11:18:40 +0100

    Add say what again

--- main.py
+++ main.py
@@ -18,6 +18,10 @@ client_data['liquidity'] = 999999.0
 client_data['investments'] = 186.6
 client_data['currency'] = 'CHF'
 
+def say_what_again():
+    text = "Say 'what' again. Say 'what' again, I dare you, I double dare you motherfucker, say what one more Goddamn time!"
+    speaker_obj.speak_text(text)
+
 def get_text_topic(speech_text):
     ratio = fuzz.partial_ratio(speech_text, "assets")
     if ratio > 90:
@@ -25,6 +29,9 @@ def get_text_topic(speech_text):
 
 def extract_text_loop():
     speech_text = listener.extract_text()
+    if speech_text.lower() == "what":
+        say_what_again()
+
     topic = get_text_topic(speech_text)
     analyze_text_loop(topic)
 
